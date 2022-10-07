2ddd77cc71c8a9fc8675a36a27457922c77176b9
Sajal Sharma <sajalsharma2408@gmail.com>
2022-10-07 20:52:57 +0530

    P04 update before battery dies

---
+++ P04_BMICalculator/script.js
@@ -0,0 +1,5 @@
+const heightValue = document.getElementById("height");
+const weightValue = document.getElementById("weight");
+const generateBtn = document.querySelector("button");
+
+generateBtn.addEventListener("onclick", () => {});
