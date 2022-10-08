bbb15dc81446b84ef743fe3d6de8505df9a83ded
Sajal Sharma <sajalsharma2408@gmail.com>
2022-10-09 00:10:59 +0530

    problem fixed

--- P04_BMICalculator/script.js
+++ P04_BMICalculator/script.js
@@ -1,12 +1,15 @@
 const heightValue = document.getElementById("height");
 const weightValue = document.getElementById("weight");
-const generateBtn = document.getElementById("button");
+const generateBtn = document.querySelector(".btn");
 
 generateBtn.addEventListener("click", () => {
   const result =
     weightValue.value / ((heightValue.value / 100) * (heightValue.value / 100));
-  console.log(heightValue.value);
-  console.log(result);
+  const n = result.toFixed(2);
+  console.log(n);
 });
 
 //here the problem I'm getting is script is loading before the page loads
+// happened when only <button></button> - getElementById("button")
+
+//problem solved when <button class="btn"></button>- querySelector(".btn")
